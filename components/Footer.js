64ece0ae522435f5ee97278a853ecc20767fe5ad
Barthélemy Jouve <jouveb@gmail.com>
2023-05-08 18:17:25 +0200

    footer

--- components/Footer.js
+++ components/Footer.js
@@ -2,8 +2,41 @@ const Footer = () => {
   return (
     <footer>
       <div className="footer-content">
+        <div className="footer-menu">
+          <div className="footer-menu-shop">
+            <h3>Best Products</h3>
+            <ul>
+              <li>Tongue Scrapper</li>
+              <li>Push-up bars</li>
+              <li>Derma Pen</li>
+            </ul>
+          </div>
+          <div className="footer-menu-blog">
+            <h3>Recent Articles</h3>
+            <ul>
+              <li>Tongue Scrapper</li>
+              <li>Push-up bars</li>
+              <li>Derma Pen</li>
+            </ul>
+          </div>
+          <div className="footer-menu-about">
+            <h3>About us</h3>
+            <ul>
+              <li>Our mission</li>
+              <li>Importance of objects</li>
+            </ul>
+          </div>
+          <div className="footer-menu-contact">
+            <h3>Get in touch</h3>
+            <ul>
+              <li>+33154332815</li>
+              <li>admin@mensessentials.com</li>
+              <li>5001 MELROSE AVE, LOS ANGELES</li>
+            </ul>
+          </div>
+        </div>
         <div className="footer-newsletter">
-          <h2>Newsletter</h2>
+          <h3>Newsletter</h3>
           <form action="#">
             <input type="email" placeholder="your@email.com"></input>
             <input type="submit" value="Suscribe"></input>
